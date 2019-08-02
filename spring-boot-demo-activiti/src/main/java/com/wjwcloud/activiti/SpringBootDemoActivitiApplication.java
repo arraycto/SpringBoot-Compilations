77ad613d3f078ac74be0770fc7c802f59bef4c87
wjw0215 <821060818@qq.com>
2019-08-02 17:04:14 +0800

    activiti

---
+++ spring-boot-demo-activiti/src/main/java/com/wjwcloud/activiti/SpringBootDemoActivitiApplication.java
@@ -0,0 +1,17 @@
+package com.wjwcloud.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JiaweiWu
+ */
+@SpringBootApplication
+public class SpringBootDemoActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoActivitiApplication.class, args);
+    }
+
+}
+
