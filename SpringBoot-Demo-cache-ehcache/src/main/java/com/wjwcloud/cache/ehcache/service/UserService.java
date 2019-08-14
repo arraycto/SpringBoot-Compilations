76db77b198e0e92af883c9dde7f7a116265af6c2
wjw0215 <821060818@qq.com>
2019-08-14 17:44:19 +0800

    ehcache

---
+++ SpringBoot-Demo-cache-ehcache/src/main/java/com/wjwcloud/cache/ehcache/service/UserService.java
@@ -0,0 +1,31 @@
+package com.wjwcloud.cache.ehcache.service;
+
+import com.wjwcloud.cache.ehcache.entity.User;
+
+/**
+ * @author JiaweiWu
+ */
+public interface UserService {
+    /**
+     * 保存或修改用户
+     *
+     * @param user 用户对象
+     * @return 操作结果
+     */
+    User saveOrUpdate(User user);
+
+    /**
+     * 获取用户
+     *
+     * @param id key值
+     * @return 返回结果
+     */
+    User get(Long id);
+
+    /**
+     * 删除
+     *
+     * @param id key值
+     */
+    void delete(Long id);
+}
