d5df810eb490bd145abf135ec98df2fc4d385163
JiaweiWu <821060818@qq.com>
2019-12-03 11:21:44 +0800

    rabbitmq

---
+++ SpringBoot-RabbitMQ/src/main/java/com/wjwcloud/rabbitmq/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.wjwcloud.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JiaweiWu
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
+
